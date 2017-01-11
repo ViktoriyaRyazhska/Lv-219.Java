cbb1ce977a4713bc5dc3447187f6b5b9dc20bcf0
Roman Karpliak <karpliakroman@gmail.com>
2017-01-11 03:29:08 +0200

    conventionv1

--- Homework/src/com/softserve/lv219/homework/Exercise88b.java
+++ Homework/src/com/softserve/lv219/homework/Exercise88b.java
@@ -1,29 +1,46 @@
+/*
+ * This software is the confidential and proprietary information 
+ * of Softserve
+ */
+
 package com.softserve.lv219.homework;
 
-public class Exercise88b  implements Executable{
+/*
+ * This class performs 88a task 
+ * of the book
+ * 
+ * @version 1.0 January 2017
+ * @author Roman Karpliak
+ * 
+ */
 
-	
-	private  int reverse(int number)
-	{
-		int reverseNumber=0;
-		while(number >0)
-		{
-			reverseNumber= reverseNumber*10 +number %10;
-			number=number/10;
-		}
-		
-		return reverseNumber;
-	}
-	
-	@Override
-	public void execute() {
-		
-		int number=123;
-		System.out.println("Reverse to "+number +" is "+reverse(number));
-	}
-	
-	
+public class Exercise88b implements Executable {
 
-	
+    @Override
+    
+    public void execute() {
+
+        int number = 123; // set a value of the number
+        
+        // print  information about number
+        System.out.println("Reverse to " + number 
+                + " is " + reverse(number));
+    }
+
+    
+    /*
+     * Returns the number in reverse order
+     * 
+     * @return the reversed number
+     */
+    private int reverse(int number) {
+
+        int reverseNumber = 0;
+        while (number > 0) {
+            reverseNumber = (reverseNumber * 10) + (number % 10);
+            number = number / 10;
+        }
+        return reverseNumber;
+    }
 
 }
