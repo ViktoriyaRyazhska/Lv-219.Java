3a6e80cc540962374cf2a3ce52f7bb75430dd50c
Vova Derkach <derkachvolody@gmail.com>
2017-01-12 11:58:34 +0200

    Add Test182, Test182_TestNg and changes in Exercise182

--- Homework/src/com/softserve/lv219/homework/Exercise182.java
+++ Homework/src/com/softserve/lv219/homework/Exercise182.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 public class Exercise182 implements Executable
 {
+	private List<Integer> listOfIntegers = new ArrayList<>();
+	public List<Integer> suitableNumbers = new ArrayList<>();
+	private int numberOfIntegers = 0;
+    private int sum = 0;
+    private int quantity = 0;
+	
     @Override
     public void execute()
     {
-        int numberOfIntegers = 0;
-        int sum = 0;
-        int quantity = 0;
-        List<Integer> listOfIntegers = new ArrayList<>();
-
         System.out.println("Enter your natural number please.");
         numberOfIntegers = Integer.parseInt(ScanerSingleton.getScanner().nextLine());
 
@@ -21,15 +22,32 @@ public class Exercise182 implements Executable
             System.out.println("Enter your integer #" + (i + 1)+ "");
             listOfIntegers.add(Integer.parseInt(ScanerSingleton.getScanner().nextLine()));
         }
-        for (int i = 0; i < listOfIntegers.size(); i++)
+        quantity = quantity(listOfIntegers);
+        sum = sum(suitableNumbers);
+        
+        System.out.println("Quantity = "+ quantity+ "");
+        System.out.println("Sum = "+ sum+ "");
+    }
+    
+    public int quantity(List<Integer> listOfIntegers) {
+    	
+    	int quantity = 0;
+    	for (int i = 0; i < listOfIntegers.size(); i++)
         {
             if (listOfIntegers.get(i) % 5 == 0 && listOfIntegers.get(i) % 7 != 0)
             {
                 quantity++;
-                sum += listOfIntegers.get(i);
+                suitableNumbers.add(listOfIntegers.get(i));
             }
         }
-        System.out.println("Quantity = "+ quantity+ "");
-        System.out.println("Sum = "+ sum+ "");
+    	return quantity;
+    }
+    
+    public int sum(List<Integer> listOfIntegers) {
+    	int sum = 0;
+    	for (int i : listOfIntegers) {
+    		sum += i;
+    	}
+    	return sum;
     }
 }
