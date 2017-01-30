41a1e44103b03840ad04f3eea27f2472297d806c
Roman Karpliak <karpliakroman@gmail.com>
2017-01-30 18:37:20 +0200

    query 2 finnished

--- src/main/java/com/softserve/edu/controller/HotelController.java
+++ src/main/java/com/softserve/edu/controller/HotelController.java
@@ -1,16 +1,33 @@
 package com.softserve.edu.controller;
 
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.softserve.edu.service.HotelService;
 
 @Controller
 public class HotelController {
 
+    @Autowired
+    private HotelService hotelService;
+
     @RequestMapping(value = { "/hotelForm" }, method = RequestMethod.GET)
     public String showForm(ModelMap model) {
         return "hotelForm";
+    }
+
+    @RequestMapping(value = "/hotels", method = RequestMethod.POST)
+    public ModelAndView getByCity(@RequestParam String city, ModelMap model) {
+
+        return new ModelAndView("hotels", "set", new HashSet<>(hotelService.findHotels(city)));
+
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/pages/hotel/showHotels.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<div>
+	
+	<table>
+	    <th>Id</th>
+		<th>HotelName</th>
+		<c:forEach items="${set}" var="hotel">
+			<tr>
+				
+				<td><c:out value="${hotel.id}" />
+				<td><c:out value="${hotel.name}" />
+				<td>
+			</tr>
+		</c:forEach>
+	</table>
+</div>
