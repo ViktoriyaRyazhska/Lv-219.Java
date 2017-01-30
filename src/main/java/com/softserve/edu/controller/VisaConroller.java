fe8cc8141fd87d6bd280d9009e5b4e253423c782
Roman Karpliak <karpliakroman@gmail.com>
2017-01-31 00:48:09 +0200

    query 5 added

--- src/main/java/com/softserve/edu/controller/VisaConroller.java
+++ src/main/java/com/softserve/edu/controller/VisaConroller.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.softserve.edu.service.VisaService;
 
@@ -17,6 +19,12 @@ public class VisaConroller {
     @RequestMapping(value = { "/visaForm" }, method = RequestMethod.GET)
     public String showForm(ModelMap model) {
         return "visaForm";
+    }
+
+    @RequestMapping(value = "/visasOfClient", method = RequestMethod.POST)
+    public ModelAndView getByClient(@RequestParam String firstName, @RequestParam String lastName, ModelMap model) {
+        return new ModelAndView("visaByClient", "set", visaService.find(firstName, lastName));
+
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/pages/visa/visaByClient.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<table>
+        <th>Country</th>
+	    <th>Start Date</th>
+		<th>End Date</th>
+		<c:forEach items="${set}" var="visa">
+			<tr>
+				<td><c:out value="${visa.country.name}" />
+				<td><c:out value="${visa.startDate}" />
+				<td><c:out value="${visa.endDate}" />
+				<td>
+			</tr>
+		</c:forEach>
+	</table>
