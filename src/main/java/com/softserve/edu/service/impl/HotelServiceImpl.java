90fcc87520eaa8d4e16fdc9621bb47e0001af0f8
ohorodnyk <o.ohorodnyk@gmail.com>
2017-01-30 17:05:12 +0200

    fix for HotelServiImpl

--- src/main/java/com/softserve/edu/service/impl/HotelServiceImpl.java
+++ src/main/java/com/softserve/edu/service/impl/HotelServiceImpl.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.softserve.edu.dao.HotelDao;
 import com.softserve.edu.domain.Hotel;
 import com.softserve.edu.service.HotelService;
 
+@Service
 public class HotelServiceImpl implements HotelService {
-
+	@Autowired
 	private HotelDao hotelDao;
 
 	@Override
@@ -47,25 +51,25 @@ public class HotelServiceImpl implements HotelService {
 
 	@Override
 	public int findCountOfFreeRooms(String hotel, Date date) {
-		
+
 		return hotelDao.findCountOfFreeRooms(hotel, date);
 	}
 
 	@Override
 	public List<Object[]> findFree(String city, Date date) {
-	
+
 		return hotelDao.findFree(city, date);
 	}
 
 	@Override
 	public int findCountOfClients(String hotel) {
-	
+
 		return hotelDao.findCountOfClients(hotel);
 	}
 
 	@Override
 	public int averageTimeOfBooking(String hotel) {
-		
+
 		return hotelDao.averageTimeOfBooking(hotel);
 	}
 
